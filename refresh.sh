45862283fe9352a4c2292d07b86a92b93f43c025
winston-bosan <winston.bosan@gmail.com>
2025-04-24 19:23:08 -0400

    WIP: JS wrangling

--- refresh.sh
+++ refresh.sh
@@ -7,7 +7,7 @@ echo "let mockData = [" >> ./data.js
 curl -s https://huggingface.co/spaces/Presidentlin/llm-pricing-calculator/resolve/main/src/lib/data.ts | tail -n +4 >> ./data.js
 # add the lines below into data.json - as a new line
 echo "console.log(JSON.stringify(mockData));" >> ./data.js
-node ./data.js >> price_data.json
+node ./data.js > price_data.json
 rm ./data.js
 
 # Rank data is a bit harder, so bear with me... This is about to get ugly
